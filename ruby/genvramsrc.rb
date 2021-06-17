ea8ac034ba3467b03675f1ec7fb5cdb1c6b6021e
yagshi <yagshi@ugsf.info>
2021-06-17 17:09:46 +0900

    genvramsrc modified

--- ruby/genvramsrc.rb
+++ ruby/genvramsrc.rb
@@ -3,19 +3,23 @@ require 'bundler/setup'
 require 'pycall/import'
 include PyCall::Import
 
+WIDTH = 384
+
 pyfrom :PIL, import: [:Image, :ImageFont, :ImageDraw]
 
 font = ImageFont.truetype(font: '/usr/share/fonts/opentype/noto/NotoSansCJK-Black.ttc', size: 14)
-image = Image.new(:RGB, [128, 16])
+image = Image.new(:RGB, [WIDTH, 16])
 draw = ImageDraw.Draw(image)
-draw.text([0, -4], '起動/system ready.', font: font)
+draw.text([0, -4], '【Wi-Fi display by Yagshi】待機中/Standing by...', font: font)
 word1 = 0
 word2 = 0
 src1 = ""
 src2 = ""
 bit = 1 << 31
 16.times {|y|
-  128.times {|x|
+  src1 = src1 + "{"
+  src2 = src2 + "{"
+  WIDTH.times {|x|
     r, g, b = image.getpixel(PyCall::Tuple.([x, y]))
     i = 0.3 * r + 0.59 * g + 0.11 * b
     if i > 120 then
@@ -33,10 +37,14 @@ bit = 1 << 31
       word2 = 0
     end
   }
-  src1 += "\n"
-  src2 += "\n"
+  src1 = src1[0..-3]  # 最後の,を抜く
+  src2 = src2[0..-3]
+  src1 += "},\n"
+  src2 += "},\n"
 }
 image.show()
+src1 = src1[0..-3]  # 最後の,を抜く
+src2 = src2[0..-3]
 puts "{\n" + src1 + "},"
 puts "{\n" + src2 + "},"
 
