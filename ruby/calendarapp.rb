099a0e3080e4757c264b02245d0cae2787d68e7c
yagshi <yagshi@ugsf.info>
2021-06-27 12:34:13 +0900

    time font is shrinked

--- ruby/calendarapp.rb
+++ ruby/calendarapp.rb
@@ -9,12 +9,14 @@ require 'google/apis/calendar_v3'
 include PyCall::Import
 
 WIDTH = 384
-KEYFILE = 'iotdisplay-405447f1d61a.json'.freeze
-CALENDAR_ID = 'kqjf08tfsnanv8p5rl2pa4p9p0@group.calendar.google.com'.freeze
+KEYFILE = 'iotdisplay-key.json'.freeze
+#CALENDAR_ID = 'kqjf08tfsnanv8p5rl2pa4p9p0@group.calendar.google.com'.freeze
+CALENDAR_ID = 'hjkoba@gmail.com'.freeze
 
 pyfrom :PIL, import: [:Image, :ImageFont, :ImageDraw]
 
-font = ImageFont.truetype(font: '/usr/share/fonts/opentype/noto/NotoSansCJK-Black.ttc', size: 14)
+font_m = ImageFont.truetype(font: '/usr/share/fonts/opentype/noto/NotoSansCJK-Black.ttc', size: 14)
+font_s = ImageFont.truetype(font: '/usr/share/fonts/opentype/noto/NotoSansCJK-Black.ttc', size: 10)
 image = Image.new(:RGB, [WIDTH, 16])
 draw = ImageDraw.Draw(image)
 
@@ -50,50 +52,56 @@ def get_calendar(service)
         str += "%02d日: " % [dt.day]
       end
     end
-    str += ev.summary
   rescue => e
     p e
   end
-  str
+  [str, ev.summary]
 end
 
 while true
-  d = Time.now
-  str = get_calendar(service) + "  "
-  puts str
+  str_tm, str_ev = get_calendar(service)
+  puts str_tm + " " + str_ev
+  str_tm = str_tm + " "
+  str_ev = str_ev + " "
   draw.rectangle([0, 0, WIDTH, 16], fill: PyCall::Tuple.([0, 0, 0, 0]))
-  text_width, _ = font.getsize(str)
-  draw.text([0, -4], str * (WIDTH / text_width), font: font)
-word1 = 0
-word2 = 0
-bit = 1 << 31
-16.times {|y|
-  str1 = ""
-  str2 = ""
-  WIDTH.times {|x|
-    r, g, b = image.getpixel(PyCall::Tuple.([x, y]))
-    i = 0.3 * r + 0.59 * g + 0.11 * b
-    if i > 120 then
-      word1 = word1 + bit
-      word2 = word2 + bit if i > 180
-    elsif i > 80 then
-      word2 = word2 + bit
-    end
-    bit = bit >> 1
-    if bit == 0 then
-      str1 += "%08x" % [word1]
-      str2 += "%08x" % [word2]
-      bit = 1 << 31
-      word1 = 0
-      word2 = 0
+  text_width_tm, _ = font_s.getsize(str_tm)
+  text_width_ev, _ = font_m.getsize(str_ev)
+  draw.text([0, 0], str_tm, font: font_s)
+  draw.text([text_width_tm, -4], str_ev, font: font_m)
+  
+  
+  word1 = 0
+  word2 = 0
+  bit = 1 << 31
+  16.times {|y|
+    str1 = ""
+    str2 = ""
+    WIDTH.times {|x|
+      r, g, b = image.getpixel(PyCall::Tuple.([x, y]))
+      i = 0.3 * r + 0.59 * g + 0.11 * b
+      if i > 120 then
+        word1 = word1 + bit
+        word2 = word2 + bit if i > 180
+      elsif i > 80 then
+        word2 = word2 + bit
+      end
+      bit = bit >> 1
+      if bit == 0 then
+        str1 += "%08x" % [word1]
+        str2 += "%08x" % [word2]
+        bit = 1 << 31
+        word1 = 0
+        word2 = 0
+      end
+    }
+    begin
+      Net::HTTP.get_response(URI.parse("http://iotdisplay.local/set?row=#{y}&plane=0&data=#{str1}"))
+      Net::HTTP.get_response(URI.parse("http://iotdisplay.local/set?row=#{y}&plane=1&data=#{str2}"))
+      Net::HTTP.get_response(URI.parse("http://iotdisplay.local/?width=#{text_width_tm + text_width_ev}"))
+      puts (text_width_tm + text_width_ev)
+    rescue => e
+      p e
     end
   }
-  begin
-    Net::HTTP.get_response(URI.parse("http://wifidisplay.local/set?row=#{y}&plane=0&data=#{str1}"))
-    Net::HTTP.get_response(URI.parse("http://wifidisplay.local/set?row=#{y}&plane=1&data=#{str2}"))
-  rescue => e
-    p e
-  end
-}
-sleep(30)
+  sleep(30)
 end
